5d04a4071560b2121220542d29634935f4b39cb3
Hodja <hojajepbar@gmail.com>
2018-03-24 21:34:15 +0200

    commit

--- business/js/scrolling.js
+++ business/js/scrolling.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+	$("#myNavbar").on('show.bs.collapse', function() {
+	    $('a').click(function() {
+	        $("#myNavbar").collapse('hide');
+	    });
+	});
 
   $('.navbar').affix({
     offset: {
@@ -42,8 +47,24 @@ $(document).ready(function(){
             $('#return-to-top').fadeOut(200);   // Else fade out the arrow
         }
     });
+
+
   AOS.init({
     duration: 500,
-    disable: window.innerWidth < 768
   });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.collapse a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.collapse ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
